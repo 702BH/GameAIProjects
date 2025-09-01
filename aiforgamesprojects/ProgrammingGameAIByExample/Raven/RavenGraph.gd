52489f287bc4e45c814096ffe245a132e5eb3e64
702BH <65957905+702BH@users.noreply.github.com>
2025-09-01 21:49:17 +0300

    implemented map saving and loading first draft

--- aiforgamesprojects/ProgrammingGameAIByExample/Raven/RavenGraph.gd
+++ aiforgamesprojects/ProgrammingGameAIByExample/Raven/RavenGraph.gd
@@ -10,8 +10,9 @@ var next_index := 0
 func _init(context_digraph:bool) -> void:
 	di_graph = context_digraph
 
-func add_vertex(type: RavenNode.NodeType, pos: Vector2, border:bool) -> void:
+func add_vertex(type: RavenNode.NodeType, pos: Vector2, border:bool) -> RavenNode:
 	var vertex = RavenNode.new(next_index, type, pos, border)
 	nodes.append(vertex)
 	edges[next_index] = []
 	next_index += 1
+	return vertex
--- aiforgamesprojects/ProgrammingGameAIByExample/Raven/map_ui_editor.gd
+++ aiforgamesprojects/ProgrammingGameAIByExample/Raven/map_ui_editor.gd
@@ -7,12 +7,13 @@ var rows := 0
 var columns := 0
 var grid_world = []
 var graph: RavenGraph
+var loaded_map := ""
 
 
 func _ready() -> void:
 	graph = RavenGraph.new(false)
 	generate_grid()
-	initialise_grid_container()
+	initialise_grid_container(rows, columns, resolution, grid_world)
 	print(grid_world[2][5])
 
 func generate_grid() -> void:
@@ -30,11 +31,11 @@ func generate_grid() -> void:
 				grid_world[i].append(graph.nodes[i * columns + j])
 
 
-func initialise_grid_container() -> void:
-	grid_container.rows = rows
-	grid_container.columns = columns
-	grid_container.resolution = resolution
-	grid_container.grid_world = grid_world
+func initialise_grid_container(_rows:int, _columns:int, _res:float, _grid:Array) -> void:
+	grid_container.rows = _rows
+	grid_container.columns = _columns
+	grid_container.resolution = _res
+	grid_container.grid_world = _grid
 	grid_container.queue_redraw()
 
 
@@ -64,7 +65,7 @@ func _on_save_pressed() -> void:
 			var node: RavenNode = grid_world[row][col]
 			save_data["nodes"].append({
 				"type": node.node_type,
-				"position": node.node_pos,
+				"position": {"x":node.node_pos.x, "y":node.node_pos.y},
 				"row": row,
 				"column": col
 			})
@@ -74,3 +75,53 @@ func _on_save_pressed() -> void:
 	var file = FileAccess.open(file_path, FileAccess.WRITE)
 	var json_string = JSON.stringify(save_data)
 	file.store_line(json_string)
+	file.close()
+
+
+func _on_load_pressed() -> void:
+	$LoadMapDialog.root_subfolder = "res://ProgrammingGameAIByExample/Raven/Maps/"
+	$LoadMapDialog.popup_centered()
+
+
+func _on_load_map_dialog_file_selected(path: String) -> void:
+	$LoadMapDialog.hide()
+	loaded_map = path
+	print(loaded_map)
+	load_world_from_file(loaded_map)
+
+
+func load_world_from_file(file_path: String) -> void:
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	if file == null:
+		push_error("Could not open file")
+		return
+	
+	var text = file.get_as_text()
+	var result = JSON.parse_string(text)
+	if result == null:
+		push_error("invalid Json")
+		return
+	
+	graph = RavenGraph.new(false)
+	grid_world = []
+	
+	var map_rows = result["rows"]
+	var map_columns = result["columns"]
+	var map_res = result["resolution"]
+	var nodes = result["nodes"]
+	
+	grid_world.resize(map_rows)
+	for i in range(map_rows):
+		grid_world[i] = []
+		grid_world[i].resize(map_columns)
+	
+	for node in nodes:
+		if node["row"] == 0 or node["row"] == map_rows-1 or node["column"]== 0 or node["column"]==map_columns-1:
+			var graph_node = graph.add_vertex(node["type"], Vector2(node["position"]["x"], node["position"]["y"]), true)
+			grid_world[node["row"]][node["column"]] = graph_node
+		else:
+			var graph_node = graph.add_vertex(node["type"], Vector2(node["position"]["x"], node["position"]["y"]), false)
+			grid_world[node["row"]][node["column"]] = graph_node
+	initialise_grid_container(map_rows, map_columns, map_res, grid_world)
+	print("completed")
+	
