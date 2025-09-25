9607f4b038ee3b90e8d592a253454056063ecb97
702BH <65957905+702BH@users.noreply.github.com>
2025-09-25 21:16:36 +0300

    implemented all needed functions for basic goals, except for distance to item

--- aiforgamesprojects/ProgrammingGameAIByExample/Raven/Goals/raven_feature.gd
+++ aiforgamesprojects/ProgrammingGameAIByExample/Raven/Goals/raven_feature.gd
@@ -2,8 +2,22 @@ class_name RavenFeature
 extends RefCounted
 
 
+static func distance_to_item(bot: RavenAgent, type: RavenNodeItem.ItemType) -> float:
+	return 0.0
+
+
+
 static func health(agent:RavenAgent) -> float:
 	return agent.health / agent.max_health
+
+
+static func individual_weapon_strength(bot: RavenAgent, weapon: RavenWeapon.WeaponType) -> float:
+	
+	var wp :RavenWeapon = bot.weapon_system.get_weapon_from_inventory(weapon)
+	
+	if wp:
+		return wp.num_rounds_left / _get_max_rounds_bot_can_carry_for_weapon(weapon, bot)
+	return 0.0
 
 
 static func total_weapon_strength(agent:RavenAgent) -> float:
@@ -19,13 +33,9 @@ static func total_weapon_strength(agent:RavenAgent) -> float:
 		total_rounds_carrying += agent.weapon_system.weapon_map.get(weapon, 0.0)
 	
 	var tweaker := 0.1
-
-	
-	
-	
-	return 0.0
+	return tweaker + (1-tweaker)*(total_rounds_carrying)/total_rounds_carryable
 
 
 
-func _get_max_rounds_bot_can_carry_for_weapon(type: RavenWeapon.WeaponType, agent:RavenAgent) -> float:
+static func _get_max_rounds_bot_can_carry_for_weapon(type: RavenWeapon.WeaponType, agent:RavenAgent) -> float:
 	return agent.stats.max_rounds.get(type, 0.0)
