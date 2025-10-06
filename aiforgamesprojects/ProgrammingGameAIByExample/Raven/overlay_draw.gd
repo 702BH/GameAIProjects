c23ecbed927a136ddcc6893586dcd68c9b2abc4f
702BH <65957905+702BH@users.noreply.github.com>
2025-10-06 11:10:41 +0300

    Implemented all weapon drawings

--- aiforgamesprojects/ProgrammingGameAIByExample/Raven/overlay_draw.gd
+++ aiforgamesprojects/ProgrammingGameAIByExample/Raven/overlay_draw.gd
@@ -1,12 +1,12 @@
 extends Node2D
 #
-#
-#func _process(delta: float) -> void:
-	#queue_redraw()
-#
-#func _draw() -> void:
-	#var mouse_position:= get_global_mouse_position()
-	#var mouse_grid = World.position_to_grid(mouse_position)
-	#var color = Color.CRIMSON
-	#color.a = 0.5
-	#draw_rect(Rect2(mouse_grid.x * World.resolution, mouse_grid.y * World.resolution, World.resolution, World.resolution), color)
+
+func _process(delta: float) -> void:
+	queue_redraw()
+
+func _draw() -> void:
+	var mouse_position:= get_global_mouse_position()
+	var mouse_grid = World.position_to_grid(mouse_position)
+	var color = Color.CRIMSON
+	color.a = 0.5
+	draw_rect(Rect2(mouse_grid.x * World.resolution, mouse_grid.y * World.resolution, World.resolution, World.resolution), color)
