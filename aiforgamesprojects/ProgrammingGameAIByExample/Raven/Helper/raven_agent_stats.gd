cf7c98fd9d0c398347c17f4f1e5d03eb6d58dbb3
702BH <65957905+702BH@users.noreply.github.com>
2025-10-17 15:28:50 +0300

    implemented shot variance based on agents accuracy

--- aiforgamesprojects/ProgrammingGameAIByExample/Raven/Helper/raven_agent_stats.gd
+++ aiforgamesprojects/ProgrammingGameAIByExample/Raven/Helper/raven_agent_stats.gd
@@ -5,3 +5,12 @@ extends RefCounted
 var max_rounds = {
 	RavenWeapon.WeaponType.SHOTGUN: 15.0
 }
+
+var accuracy := 70.0
+
+
+
+func accuracy_to_variance(min_spread:float, max_spread:float) -> float:
+	var factor = 1.0 - clamp(accuracy / 100.0,0.0, 1.0)
+	#print("Factor: ", factor)
+	return lerp(min_spread, max_spread, factor)
