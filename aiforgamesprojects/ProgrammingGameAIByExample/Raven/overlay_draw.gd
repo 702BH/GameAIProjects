cef3053bb63fac7625475fb0fa140d15a59ce422
702BH <65957905+702BH@users.noreply.github.com>
2025-10-28 12:07:01 +0200

    drawing normals

--- aiforgamesprojects/ProgrammingGameAIByExample/Raven/overlay_draw.gd
+++ aiforgamesprojects/ProgrammingGameAIByExample/Raven/overlay_draw.gd
@@ -10,3 +10,11 @@ func _draw() -> void:
 	var color = Color.CRIMSON
 	color.a = 0.5
 	draw_rect(Rect2(mouse_grid.x * World.resolution, mouse_grid.y * World.resolution, World.resolution, World.resolution), color)
+	
+	for key in World.cell_buckets_static:
+			var walls = World.cell_buckets_static[key]
+			for wall in walls:
+				if !wall.is_border:
+					var wall_segments = wall.wall_segments
+					for j in range(wall_segments.size()):
+						draw_line(wall_segments[j][0], wall_segments[j][0] + wall.wall_normals[j] * 5.0, Color.BLUE)
